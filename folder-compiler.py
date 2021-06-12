29fadf81e263fb83c69d11551b9a84822b138644
Sparib <52337413+Sparib@users.noreply.github.com>
2021-06-12 14:52:07 -0400

    Slight fixes

--- folder-compiler.py
+++ folder-compiler.py
@@ -6,11 +6,6 @@ def get_subdirs(directory: str) -> List[str]:
     return list(filter(lambda v: os.path.isdir(v), os.listdir(directory)))
 
 def main():
-    try:
-        shutil.rmtree("./compiled-folder")
-    except:
-        pass
-
     # Throw if the folder that we want to use already exists
     if os.path.exists("./compiled-folder"):
         print("./compiled-folder exists! Please remove this folder so the program can work properly!")
@@ -31,18 +26,22 @@ def main():
         tgt = src.split("/"); tgt[1] = "compiled-folder"; tgt = "/".join(tgt)
         os.makedirs(os.path.dirname(tgt), exist_ok=True)
         if os.path.isfile(tgt):
+            # Append data if file already exists
             try:
                 sf = open(src, 'r')
                 tf = open(tgt, "a")
                 tf.write("\n" + sf.read())
             except:
+                # Throw and remove tree
                 shutil.rmtree("./compiled-folder")
                 print("Error while appending file data!")
                 exit(1)
             finally:
+                # Close files
                 sf.close()
                 tf.close()
         else:
+            # Copy files
             shutil.copy(src, tgt)
 
 if __name__ == "__main__":
