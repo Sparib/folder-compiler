4e28ca70ac221e8cdb1926340bb23bf1a527e044
Sparib <52337413+Sparib@users.noreply.github.com>
2021-06-12 14:46:40 -0400

    Main update works

--- folder-compiler.py
+++ folder-compiler.py
@@ -1,10 +1,16 @@
 import os
+import shutil
 from typing import List
 
-def filter_func(var):
-    return os.path
+def get_subdirs(directory: str) -> List[str]:
+    return list(filter(lambda v: os.path.isdir(v), os.listdir(directory)))
 
 def main():
+    try:
+        shutil.rmtree("./compiled-folder")
+    except:
+        pass
+
     # Throw if the folder that we want to use already exists
     if os.path.exists("./compiled-folder"):
         print("./compiled-folder exists! Please remove this folder so the program can work properly!")
@@ -12,10 +18,32 @@ def main():
     os.mkdir("./compiled-folder")
     
     # Get all of the folders within the root directory
-    root_folders = list(filter(lambda v: os.path.isdir(v), os.listdir(".")))
-    root_folders.remove("compiled-folder")
-    print(root_folders)
-    
+    file_structure = []
+    for root, _, files in os.walk("."):
+        if root == ".": continue
+        for name in files:
+            file_structure.append(os.path.join(root, name).replace("\\", "/"))
+
+    print(file_structure)
+
+    for src in file_structure:
+        # Set up target directory for copying
+        tgt = src.split("/"); tgt[1] = "compiled-folder"; tgt = "/".join(tgt)
+        os.makedirs(os.path.dirname(tgt), exist_ok=True)
+        if os.path.isfile(tgt):
+            try:
+                sf = open(src, 'r')
+                tf = open(tgt, "a")
+                tf.write("\n" + sf.read())
+            except:
+                shutil.rmtree("./compiled-folder")
+                print("Error while appending file data!")
+                exit(1)
+            finally:
+                sf.close()
+                tf.close()
+        else:
+            shutil.copy(src, tgt)
 
 if __name__ == "__main__":
     main()
